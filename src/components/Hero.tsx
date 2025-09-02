d6dfe0907a12e819f15d237754d50febb1fa49af
shivakalluri-engagebay <shiva@gmail.com>
2025-09-02 10:34:00 +0530

    Fix: Remove unnecessary empty line in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,6 +30,7 @@ export default function Hero() {
           >
             {SITE.tagline}
           </motion.p>
+
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
